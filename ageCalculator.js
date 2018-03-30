770f73dbb37b0d5cb63a7393b2d27b13a5333da1
Devon Norris <norris.devon@gmail.com>
2018-03-30 15:31:13 +0000

    age calculator problem

--- ageCalculator.js
+++ ageCalculator.js
@@ -1,10 +1,3 @@
-function isOdd(num) {
-  return num % 2 !== 0;
-}
-
-console.log("3 is odd: " + isOdd(3));
-console.log("8 is odd: " + isOdd(8));
-
 function ageCalculator(name, yearOfBirth, currentYear) {
   var age = currentYear - yearOfBirth;
   return name + " is " + age + " years old.";
