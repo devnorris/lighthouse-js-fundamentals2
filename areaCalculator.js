ca07b8eaba0c1faf7c7f48e343dde90f66bc7ab6
Devon Norris <norris.devon@gmail.com>
2018-03-31 14:47:43 +0000

    Area calculator practice js

--- areaCalculator.js
+++ areaCalculator.js
@@ -1,15 +1,3 @@
-var temperature = 12;
-
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
-}
-
-console.log("Now you're ready to go outside!");
-
 function calculateRectangleArea(length, width) {
   if (length < 0|| width < 0 ) {
     return undefined;
