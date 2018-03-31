3fcf7c10ee79158ca56b66dbb910447027036f55
Devon Norris <norris.devon@gmail.com>
2018-03-31 00:33:45 +0000

    Adds conditional exercises

--- odd.js
+++ odd.js
@@ -4,14 +4,3 @@ function isOdd(num) {
 
 console.log("3 is odd: " + isOdd(3));
 console.log("8 is odd: " + isOdd(8));
-
-function howManyHundreds(num) {
-  var result = Math.floor(num / 100);
-  return result;
-}
-
-console.log(howManyHundreds(1000), "=?", 10);
-console.log(howManyHundreds(894), "=?", 8);
-console.log(howManyHundreds(520), "=?", 5);
-console.log(howManyHundreds(99), "=?", 0);
-console.log(howManyHundreds(0), "=?", 0);
